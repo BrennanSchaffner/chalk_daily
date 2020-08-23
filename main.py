bd0c9b032e015f1e61a7c323b0119c74662aa3a2
BrennanSchaffner <49691845+BrennanSchaffner@users.noreply.github.com>
2020-08-22 22:27:36 -0500

    testing pygame basic

--- main.py
+++ main.py
@@ -8,22 +8,61 @@ from sheet_reader import SheetReader
 from datetime import date
 import calendar
 import pygame as pg
+import pygame ######################
 import easygui
 import sys
 import os.path
 
+# this means debug lines #######################################
 
 def main():
-    get_sheet_url()
+    url = get_sheet_url()
     print("atleast it ran")
-    display()
+    #display()
+    test_display()
 
 
 def get_sheet_url():
     url = easygui.enterbox("Your Google sheet URL")
     if url == '' or url is None:
         sys.exit()
+    return url
 
+def test_display(): #####################
+    successes, failures = pygame.init()
+    print("{0} successes and {1} failures".format(successes, failures))
+
+    screen = pygame.display.set_mode((720, 480))
+    clock = pygame.time.Clock()
+    FPS = 60  # Frames per second.
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    # RED = (255, 0, 0), GREEN = (0, 255, 0), BLUE = (0, 0, 255).
+
+    rect = pygame.Rect((0, 0), (32, 32))
+    image = pygame.Surface((32, 32))
+    image.fill(WHITE)
+
+    while True:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    rect.move_ip(0, -2)
+                elif event.key == pygame.K_s:
+                    rect.move_ip(0, 2)
+                elif event.key == pygame.K_a:
+                    rect.move_ip(-2, 0)
+                elif event.key == pygame.K_d:
+                    rect.move_ip(2, 0)
+
+        screen.fill(BLACK)
+        screen.blit(image, rect)
+        pygame.display.update()  # Or pygame.display.flip()
 
 def display():
     pg.init()
